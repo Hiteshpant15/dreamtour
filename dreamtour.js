53a836a599af5a7c437c44832498af15d044758e
Hiteshpant15 <hpant245@gmail.com>
2025-05-06 17:40:17 +0530

    commit

---
+++ dreamtour.js
@@ -0,0 +1,6 @@
+// function ans() {
+//     document.getElementById(hphp).innerHTML="We offer a wide range of tours, including cultural, adventure, luxury, and customized itineraries.Popular destinations include Europe, Africa (e.g., Morocco)"
+// }
+// var ans = (aa) => {
+//     document.write( "We offer a wide range of tours, including cultural, adventure, luxury, and customized itineraries.Popular destinations include Europe, Africa (e.g., Morocco)")
+// }
